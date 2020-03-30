11a10179596feed0303757be95724d50d39aca40
lookwhoistalkingpython <lookwhoistalkingpython@gmail.com>
2020-03-30 20:35:51 +0100

    More work on UVM component tablets.

--- scratch/scratch1.py
+++ scratch/scratch1.py
@@ -1,11 +1,4 @@
-//Example: subcomponent.port.connect(port);
-[component instance].[port name].connect([port name]);
+class [config name] extends uvm_object;
+ `uvm_object_utils([config name])
 
-//Example: export.connect(subcomponent.export);
-[export instance].connect([subcomponent.export instance]);
-
-if (!uvm_config_db#([element type])::get(this, "", "[element type]", [element instance]))
- `uvm_fatal("[parent name]","Could not get handle to [element type].")
-
-//Example: comp1.port.connect(comp2.export);
-[component instance].[port name].connect([component instance].[export/import name]);
+endclass
