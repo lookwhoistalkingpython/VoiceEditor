1db0ce3fb11febab595d230036e88fbe6582e257
lookwhoistalkingpython <lookwhoistalkingpython@gmail.com>
2020-04-21 16:13:29 +0100

    Updated scratch1

--- scratch/scratch1.py
+++ scratch/scratch1.py
@@ -6,12 +6,16 @@ hello
 how
 hello
 how
+print("debug: some debug message")
 hello
+how
+hello
+print("debug: some debug message")
 how
 hello
 how
 hello
 how
-hello
-how
+print("debug: some debug message")
+print("debug: some debug message")
 
